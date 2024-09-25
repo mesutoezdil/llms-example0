58b1eaa337b89d4a0f6305fd5ec906bbdb6f8486
mesutoezdil <mesudozdil@gmail.com>
2024-09-25 10:21:04 +0200

    Initial commit of go-client-example

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-client-example
+
+go 1.22.6
+
+require github.com/predictionguard/go-client v0.24.0 // indirect
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/predictionguard/go-client"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func run() error {
+	host := "https://api.predictionguard.com"
+	apiKey := os.Getenv("PGKEY")
+
+	logger := func(ctx context.Context, msg string, v ...any) {
+		s := fmt.Sprintf("msg: %s", msg)
+		for i := 0; i < len(v); i = i + 2 {
+			s = s + fmt.Sprintf(", %s: %v", v[i], v[i+1])
+		}
+		log.Println(s)
+	}
+
+	cln := client.New(logger, host, apiKey)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	input := client.CompletionInput{
+		Model:       client.Models.Hermes2ProLlama38B,
+		Prompt:      "The best thing about gophers is: ",
+		MaxTokens:   100,
+		Temperature: 0.75,
+	}
+
+	resp, err := cln.Completions(ctx, input)
+	if err != nil {
+		return fmt.Errorf("ERROR: %w", err)
+	}
+
+	fmt.Println(resp.Choices[0].Text)
+
+	return nil
+}
